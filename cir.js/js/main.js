13c2d9362fbb623cbe1f57b47c524332d435b616
AtsushiM <at.mizoue@gmail.com>
2013-01-04 11:37:23 +0900

    update

--- cir.js/js/main.js
+++ cir.js/js/main.js
@@ -1,28 +1,19 @@
 (function() {
     var $ = C.selector,
-        $dl = $('#main dl'),
-        e = new C.Event(),
-        i = 0,
-        len = $dl.length;
+        $dt = $('#main dl dt'),
+        e = new C.Event();
 
-    for (; i < len; i++) {
-        bind($dl[i]);
-    }
-
-    function bind(element) {
-        var $dl = $(element),
-            $dt = $dl.find('dt'),
+    $dt.on(e.switchclick, function() {
+        var $dl = $(this).parent(),
             $init = $dl.find('.init'),
             cls = 'open';
 
-        $dt.on(e.switchclick, function() {
-            $dl.toggleClass(cls);
-            if ($dl.hasClass(cls)) {
-                $init.show();
-            }
-            else {
-                $init.hide();
-            }
-        });
-    }
+        $dl.toggleClass(cls);
+        if ($dl.hasClass(cls)) {
+            $init.show();
+        }
+        else {
+            $init.hide();
+        }
+    });
 }());
