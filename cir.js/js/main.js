232326da2ea0b0c54784b7452895836b2ac7e796
AtsushiM <at.mizoue@gmail.com>
2013-01-07 12:52:53 +0900

    update

--- cir.js/js/main.js
+++ cir.js/js/main.js
@@ -1,17 +1,48 @@
 (function() {
     var $ = C.selector,
-        $main = $('#main'),
-        $click = $main.find('dt, h3'),
-        e = new C.Event();
+        storage = new C.LocalStorage(),
+        clsOpen = 'open',
+        key = clsOpen + '-';
 
-    $click.on(e.switchclick, function() {
-        open(this);
+    initOpen();
+
+    // toggle
+    $('#main').find('dt, h3').on(C.event.click, function() {
+        var $parent = $(this).parent();
+
+        if (!$parent.hasClass(clsOpen)) {
+            return addOpen($parent);
+        }
+
+        return removeOpen($parent);
     });
 
-    function open(_this) {
-        var $parent = $(_this).parent(),
-            cls = 'open';
+    // local link
+    $('a[href^="#"]').on(C.event.click, function() {
+        addOpen($($(this).attr('href')));
+    });
 
-        $parent.toggleClass(cls);
+    // init open
+    function initOpen() {
+        var opendata = storage.get(),
+            i,
+            id;
+
+        for (i in opendata) {
+            id = i.split(key)[1];
+
+            if (id) {
+                addOpen($('#' + id));
+            }
+        }
+    }
+
+    function addOpen($el) {
+        $el.addClass(clsOpen);
+        storage.set(key + $el.attr('id'), 1);
+    }
+    function removeOpen($el) {
+        $el.removeClass(clsOpen);
+        storage.remove(key + $el.attr('id'));
     }
 }());
