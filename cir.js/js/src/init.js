6e19c5967988d378372cce68261d50f3ad0684a3
AtsushiM <at.mizoue@gmail.com>
2013-01-09 11:59:46 +0900

    update

--- cir.js/js/src/init.js
+++ cir.js/js/src/init.js
@@ -2,9 +2,18 @@ DOC.init = function(config) {
     'use strict';
 
     var opendata = storage.get(),
+        hash = location.hash.split('#')[1],
         i;
 
     for (i in opendata) {
-        addOpen($('#' + i));
+        open(i);
+    }
+
+    if (hash) {
+        open(hash);
+    }
+
+    function open(key) {
+        addOpen($('#' + key));
     }
 };
--- cir.js/js/src/src.combine.js
+++ cir.js/js/src/src.combine.js
@@ -22,10 +22,19 @@ DOC.init = function(config) {
     'use strict';
 
     var opendata = storage.get(),
+        hash = location.hash.split('#')[1],
         i;
 
     for (i in opendata) {
-        addOpen($('#' + i));
+        open(i);
+    }
+
+    if (hash) {
+        open(hash);
+    }
+
+    function open(key) {
+        addOpen($('#' + key));
     }
 };
 DOC.localLink = function(config) {
