03c68280917e150ebd765aeca2e682132b448d58
AtsushiM <at.mizoue@gmail.com>
2013-01-09 10:26:46 +0900

    update

--- cir.js/optimize/js/src/Ajax.js
+++ cir.js/optimize/js/src/Ajax.js
@@ -8,6 +8,7 @@ Global.Ajax = function(config) {
 
                 ajax.request({
                     url: vars.src,
+                    query: '?update=20130109',
                     callback: function(data) {
                         vars.result[vars.src] = encodeURIComponent(data);
                         vars.callback();
