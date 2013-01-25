8672b340bac943f873a57640b88cfd9a9a74bd52
AtsushiM <asionfb@gmail.com>
2013-01-25 21:03:19 +0900

    update

--- cir.js/optimize/src/DeviceShake.js
+++ cir.js/optimize/src/DeviceShake.js
@@ -1,17 +1,21 @@
 /* Test: "%JASMINE_TEST_PATH%" */
 (function() {
 var Shake,
-    mode,
+    convert,
     mobile = new C['Mobile']();
 
 if (mobile['isMobile']()) {
     if (Global['DeviceOrientation']['support']) {
         Shake = Global['DeviceOrientation'];
-        mode = 'DeviceOrientation';
+        convert = function(e) {
+            return e;
+        };
     }
     else if (Global['DeviceMotion']['support']) {
         Shake = Global['DeviceMotion'];
-        mode = 'DeviceMotion';
+        convert = function(e) {
+            return e['rotationRate'];
+        };
     }
 }
 mobile = mobile['dispose']();
@@ -25,40 +29,47 @@ if (!Shake) {
 
 Global['DeviceShake'] = klass({
     'extend': Base,
-    'init': function() {
+    'init': function(config) {
         this['_super']();
         this._shaker = new Shake();
+        this._limit = config['limit'];
+        this._waittime = config['waittime'];
+        this._callback = config['callback'];
+
+        this['bind']();
     },
     'properties': {
         'dispose': function() {
             this['unbind']();
             this._orgdis();
         },
-        'bind': function(handler) {
-            var wraphandle;
-            if (mode === 'DeviceOrientation') {
+        'bind': function() {
+            var mine = this,
+                base_e,
+                shaked = FALSE,
+                diffbeta,
                 wraphandle = function(e) {
-                    e = {
-                        'alpha': e['alpha'],
-                        'beta': e['beta'],
-                        'gamma': e['gamma']
-                    };
-                    handler(e);
-                };
-            }
-            else {
-                wraphandle = function(e) {
-                    e = e['rotationRate'];
-                    e = {
-                        'alpha': e['alpha'],
-                        'beta': e['beta'],
-                        'gamma': e['gamma']
-                    };
-                    handler(e);
-                };
-            }
+                    e = convert(e);
 
-            this._shaker['bind'](wraphandle);
+                    if (!base_e) {
+                        base_e = e;
+                    }
+
+                    diffbeta = Math.abs(e['beta'] - base_e['beta']);
+
+                    if (diffbeta > mine._limit) {
+                        shaked = TRUE;
+                        base_e = NULL;
+
+                        mine._callback(e);
+
+                        setTimeout(function() {
+                            shaked = FALSE;
+                        }, mine._waittime);
+                    }
+                };
+
+            mine._shaker['bind'](wraphandle);
         },
         'unbind': function() {
             this._shaker['unbind']();
